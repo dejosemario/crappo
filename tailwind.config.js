169365fd78ec51002a359041b10c5d96e598a4ac
dejosemario <josemariaodumodu@gmail.com>
2022-09-06 14:38:46 +0100

    set up tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,46 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    color: {
+      blue: {
+        900: '#0D0D2B',
+        600: '#2B076E',
+        500: '#491F98',
+        400: '#35068C',
+        300: '#4610AD',
+        100: '#3671E9',
+        10: '#E0E0E0',
+      },
+      grey: '#828282'
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        sm: '0',
+        lg: '0',
+        xl: '0',
+        '2xl': '0',
+      },
+      screens: {
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1140px',
+        '2xl': '1260px',
+      },
+    },
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1200px',
+      '2xl': '1440px',
+    },
+
     extend: {},
   },
   plugins: [],
 }
+
+
