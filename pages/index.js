91506b7b3994757ffad98e3a4a20b070250b0902
dejosemario <josemariaodumodu@gmail.com>
2022-09-08 10:29:55 +0100

    testing the layout

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,17 @@ import { AiOutlineRight } from "react-icons/ai";
 import Links from "../src/components/atoms/Link";
 import Text from "../src/components/atoms/Text";
 import { Heading } from "../src/components/atoms/Heading/Heading";
+import Layout from "../src/components/templates/pages";
 
 export default function Home() {
   return (
+    <Layout>
       <div className="container">
-          <Heading level={2}>Welcome</Heading>
-          <Button withIcon={true}>Login</Button>
-          <Links url="#">Click Here</Links>
-          <Text>I am emeka</Text>
+        <Heading level={2}>Welcome</Heading>
+        <Button withIcon={true}>Login</Button>
+        <Links url="#">Click Here</Links>
+        <Text>I am emeka</Text>
       </div>
+    </Layout>
   );
 }
