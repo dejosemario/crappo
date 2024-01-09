8f61088ada4fd453dcaa5f8953c6c32656232b30
dejosemario <josemariaodumodu@gmail.com>
2024-01-09 20:36:36 +0100

    fix: add some of the image and dependices needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/atom/Button/Button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Welcome to crappo app</div>
+      <Button withIcon >Click me</Button>
     </main>
   )
 }
