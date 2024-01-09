d2628dc6b1917bcc5b3435a725ef77d4e2eea998
dejosemario <josemariaodumodu@gmail.com>
2024-01-09 19:14:31 +0100

    feat: create button component for all buttons

---
+++ src/components/atom/Button/Button.tsx
@@ -0,0 +1,30 @@
+import { AiOutlineRight } from "react-icons/ai";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  withIcon?: boolean | false;
+  outlined?: boolean;
+  hover?: boolean | true;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  withIcon,
+  outlined,
+  hover,
+}) => {
+  return (
+    <button
+      className={` ${
+        outlined ? " text-black bg-white" : "text-white"
+      } py-2 px-4 rounded-full flex flex-row items-center bg-[#3671E9]`}
+    >
+      <span className={`${hover && "hover:opacity-100"} `}>{children}</span>
+      {withIcon && (
+        <span className={"bg-white p-1 ml-4 rounded-full text-[#3671E9] "}>
+          <AiOutlineRight />
+        </span>
+      )}
+    </button>
+  );
+};
