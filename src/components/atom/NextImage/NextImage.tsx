b7e4163284aa6fb5974bd7728a3be5f68cee8e06
dejosemario <josemariaodumodu@gmail.com>
2024-01-11 23:27:44 +0100

    feat: create the image component

---
+++ src/components/atom/NextImage/NextImage.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+interface NextImageProps {
+    width?: number;
+    height?: number;
+    auto?: boolean;
+    imgUrl: string;
+    className?: string;
+    alt: string;
+}
+
+export const NextImage = ({ width, height, auto, imgUrl, className, alt }: NextImageProps) => {
+    return (
+        <Image
+            alt={alt}
+            src={imgUrl}
+            className={className}
+            width={width}
+            height={height}
+            layout={`${auto ? 'fill' : 'intrinsic'}`}
+        />
+    );
+};
+
