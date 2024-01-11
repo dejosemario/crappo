066f8e05d30f4aa874a0316b13ecd2f70069513e
dejosemario <josemariaodumodu@gmail.com>
2024-01-11 23:27:57 +0100

    feat: create the heading component

---
+++ src/components/atom/Heading/Heading.tsx
@@ -0,0 +1,48 @@
+interface HeadingProps {
+  children: React.ReactNode;
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  className?: string;
+}
+
+export const Heading = ({ children, level, className }: HeadingProps) => {
+  switch (level) {
+    case 1:
+      return (
+        <h1 className={`font-bold text-[4rem] leading-normal ${className}`}>
+          {children}
+        </h1>
+      );
+    case 2:
+      return (
+        <h2 className={`font-bold text-[2.5rem] leading-15 ${className}`}>
+          {children}
+        </h2>
+      );
+    case 3:
+      return (
+        <h3 className={`font-bold text-[2rem] leading-12 ${className}`}>
+          {children}
+        </h3>
+      );
+    case 4:
+      return (
+        <h4 className={`font-bold text-[1.25rem] leading-normal ${className}`}>
+          {children}
+        </h4>
+      );
+    case 5:
+      return (
+        <h5 className={`font-bold text-[1.rem] leading-7 ${className}`}>
+          {children}
+        </h5>
+      );
+    case 6:
+      return (
+        <h6 className={`font-bold text-[1.125rem] leading-9 ${className}`}>
+          {children}
+        </h6>
+      );
+    default:
+      break;
+  }
+};
