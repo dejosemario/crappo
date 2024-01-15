fd0521002684e5c2f62ffe2ea5e3d97e465d7735
dejosemario <josemariaodumodu@gmail.com>
2024-01-15 19:28:33 +0100

    feat: create the IvCard component

---
+++ src/components/molecules/IvCard/IvCard.tsx
@@ -0,0 +1,47 @@
+import { NextImage } from "@/components/atom/NextImage/NextImage";
+import Barchart from "/src/public/images/barchart.svg";
+import People from "/src/public/images/people.svg";
+import Earth from "/src/public/images/earth.svg";
+import Text from "@/components/atom/Text/Text";
+import { Heading } from "@/components/atom/Heading/Heading";
+
+const Ivcard = () => {
+  const Ivdatas = [
+    {
+      imgUrl: Barchart,
+      alt: "Digit-currencey-Icon",
+      heading: "$30B",
+      text: "Digital Currency Exchanged",
+    },
+    {
+      imgUrl: People,
+      alt: "Digit-currencey-Icon",
+      heading: "10M+",
+      text: "Trusted Wallets Investor",
+    },
+    {
+      imgUrl: Earth,
+      alt: "Digit-currencey-Icon",
+      heading: "195",
+      text: "Countries Supported",
+    },
+  ];
+
+  return (
+    <div className="flex justify-between pt-[239px]">
+      {Ivdatas.map(({imgUrl, alt, heading, text}, idx) => (
+        <div className="flex gap-6 items-center" key={idx}>
+          <div>
+            <NextImage imgUrl={imgUrl} alt={alt} />
+          </div>
+          <div>
+            <Heading level={2}>{heading}</Heading>
+            <Text>{text}</Text>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Ivcard;
