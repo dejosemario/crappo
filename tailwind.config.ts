60f480dd5c2468c77e29f14f2571b0c3e82cc11f
dejosemario <josemariaodumodu@gmail.com>
2024-01-15 19:29:15 +0100

    fix: adding the window container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: "7.5rem",
+      center: true,
+    },
+    screens: {
+      xs: "450px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1024px",
+      xxl: "1280px",
+      "@desktop": "1440px",
+      "2xl": "1536px",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
         inter: ["var(--font-inter)"],
       },
       backgroundColor: {
-        'white-10' : 'rgba(255, 255, 255, 0.10)',
-      }
+        "white-10": "rgba(255, 255, 255, 0.10)",
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
