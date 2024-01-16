57503c08a1402befe5f52bade9d13445e1eda0ff
dejosemario <josemariaodumodu@gmail.com>
2024-01-16 13:49:49 +0100

    feat: create Illustration component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import { Button } from "@/components/atom/Button/Button";
 import Ivcard from "@/components/molecules/IvCard/IvCard";
 import Footer from "@/components/organisms/Layouts/FooterLayout/Footer";
 import HeroLayout from "@/components/organisms/Layouts/HeroSectionLayout/HeroLayout";
+import IllustrationLayout from "@/components/organisms/Layouts/IllustrationLayout/IllustrationLayout";
 import Header from "@/components/organisms/Layouts/Navlayout/Header";
 import NavLayout from "@/components/organisms/Layouts/Navlayout/Header";
 import Image from "next/image";
 
 export default function Home() {
-  return ( 
+  return (
     <main className="container mt-[45px]  flex flex-col ">
       <Header />
       <HeroLayout />
+      <IllustrationLayout />
       <Footer />
     </main>
   );
---
+++ src/components/organisms/Layouts/IllustrationLayout/IllustrationLayout.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/atom/Button/Button";
+import { Heading } from "@/components/atom/Heading/Heading";
+import { NextImage } from "@/components/atom/NextImage/NextImage";
+import Text from "@/components/atom/Text/Text";
+import Illustration from "@/public/images/illustrations.svg";
+import Dgradient from "@/public/images/d.svg";
+import Ugradient from "@/public/images/u.svg";
+
+const IllustrationLayout = () => {
+  return (
+    <div className="flex justify-between mt-[160px] relative">
+      <div className=" flex items-start">
+        <NextImage imgUrl={Illustration} alt="Illustration-image" />
+      </div>
+      <div className="pt-20">
+        <Heading level={2} className="max-w-[480px]">
+          Why you should choose CRAPPO
+        </Heading>
+        <Text className="max-w-[408px] pb-8">
+          Experience the next generation cryptocurrency platform. No financial
+          borders, extra fees, and fake reviews.
+        </Text>
+        <Button>Learn More</Button>
+      </div>
+      <div className="absolute right-[-10vw] top-[-180px]">
+        <NextImage imgUrl={Dgradient} alt="Dgradient" />
+      </div>
+      <div className="absolute right-[-10vw] top-[380px]">
+        <NextImage imgUrl={Ugradient} alt="ugradient" />
+      </div>
+    </div>
+  );
+};
+
+export default IllustrationLayout;
