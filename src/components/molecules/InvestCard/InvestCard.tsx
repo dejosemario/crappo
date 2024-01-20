9854a3633280cb7243425219a27aee8c7bf8c196
dejosemario <josemariaodumodu@gmail.com>
2024-01-20 23:42:29 +0100

    fix: positioning the div to the parent and adding the picture needed

--- src/components/molecules/InvestCard/InvestCard.tsx
+++ src/components/molecules/InvestCard/InvestCard.tsx
@@ -18,36 +18,40 @@ export default function InvestCard() {
           <Button>Learn More</Button>
         </div>
       </div>
-      <div className="flex flex-col bg-[#35068C] w-[580px] h-[298px] rounded-2xl p-[40px]">
-        <div className="flex justify-between mb-6">
-          <div className="flex gap-4">
-            <div>
-              <NextImage
-                imgUrl={Bitcoin}
-                alt={"bitcoin-logo"}
-                width={40}
-                height={40}
-              />
-            </div>
-            <div className="gap-y-1">
-              <div className="flex items-center justify-center gap-2">
-                <Heading level={5} className="text-[24px]">
-                  Bitcoin
-                </Heading>
-                <div className="text-gray-400 text-[16px] font-medium ">
-                  BTC
+      <div className="relative pb-[249px]">
+        <div className="flex flex-col bg-[#35068C] w-[580px] h-[298px] rounded-2xl p-[40px]">
+          <div className="flex justify-between mb-6">
+            <div className="flex gap-4">
+              <div>
+                <NextImage
+                  imgUrl={Bitcoin}
+                  alt={"bitcoin-logo"}
+                  width={40}
+                  height={40}
+                />
+              </div>
+              <div className="gap-y-1">
+                <div className="flex items-center justify-center gap-2">
+                  <Heading level={5} className="text-[24px]">
+                    Bitcoin
+                  </Heading>
+                  <div className="text-gray-400 text-[16px] font-medium ">
+                    BTC
+                  </div>
+                </div>
+                <div className="text-gray-400 text-[14px] font-normal ">
+                  0.00080 BTC
                 </div>
               </div>
-              <div className="text-gray-400 text-[14px] font-normal ">
-                0.00080 BTC
-              </div>
             </div>
+            <Text className="text-sm text-[#42FFFF]">+125%</Text>
           </div>
-          <Text className="text-sm text-[#42FFFF]">+125%</Text>
+          <div>
+            <NextImage imgUrl={WaterLevel} alt={"water-logo"} />
+          </div>
         </div>
-        <div>
-          <NextImage imgUrl={WaterLevel} alt={"water-logo"} />
-        </div>
+        <div className="absolute rounded-2xl w-[178px] h-[184px] bg-[#491F98] transform -translate-y-1/4 left-[64px]"></div>
+        <div className=" rounded-2xl w-[169px] h-[84px] bg-[#491F98] absolute  right-[56px] transform -translate-y-1/2"></div>
       </div>
     </div>
   );
